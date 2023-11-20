326ca88012527af720b89af5e8df1daf81386dee
NichoUlloa <n.ulloa06@ufromail.cl>
2023-11-20 15:09:55 -0300

    Se crea la clase Animal la cual debe heredar Perro y Gato

--- src/main/java/Perro.java
+++ src/main/java/Perro.java
@@ -8,3 +8,4 @@ public class Perro  extends Animal{
         System.out.println("Guau");
     }
 }
+
--- src/main/java/test/Principal.java
+++ src/main/java/test/Principal.java
@@ -3,5 +3,6 @@ package test;
 public class Principal {
     public static void main(String[] args) {
 
+
     }
 }
